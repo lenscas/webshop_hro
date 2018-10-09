b23b0f504fc85d7245b2a78f48c9a0a029e5d5e2
Nigel Huijts <nigelhuijts@hotmail.nl>
2018-10-09 15:04:01 +0200

    Login
    Login
    
    Co-Authored-By: nigelhuijts <nigelhuijts@users.noreply.github.com>

--- src/components/routes.tsx
+++ src/components/routes.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { Route, Switch } from "react-router-dom";
 import BasicComponent from "../types/basicComponent";
 import Register from "../pages/Register";
+import Login from "../pages/LoginPage";
 export default class Routes extends BasicComponent {
 	public render(){
 		return (
@@ -9,6 +10,7 @@ export default class Routes extends BasicComponent {
 				<Switch>
 					<Route exact={true} path="/" ><h1>Test</h1></Route>
 					<Route exact={true} path="/Register"><Register APIS={this.props.APIS}/></Route>
+					<Route exact={true} path="/Login"><Login APIS={this.props.APIS}/></Route>
 				</Switch>
 
 			</div>
