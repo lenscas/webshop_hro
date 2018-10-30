00dde89f59404020faa704983d98ee40b43e6a27
Dines123 <34677908+Dines123@users.noreply.github.com>
2018-10-30 13:19:23 +0100

    homepage login and register html and css

--- src/components/routes.tsx
+++ src/components/routes.tsx
@@ -4,13 +4,14 @@ import BasicComponent from "../types/basicComponent";
 import Register from "../pages/Register";
 import ListTest from "../pages/ListTest";
 import Login from "../pages/LoginPage";
+import Home from "src/pages/HomePage";
 
 export default class Routes extends BasicComponent {
 	public render(){
 		return (
 			<div>
 				<Switch>
-					<Route exact={true} path="/" ><h1>Test</h1></Route>
+					<Route exact={true} path="/" ><Home/></Route>
 					<Route exact={true} path="/Register"><Register APIS={this.props.APIS}/></Route>
 					<Route exact={true} path="/ListTest"><ListTest APIS={this.props.APIS}/></Route>
 					<Route exact={true} path="/Login"><Login APIS={this.props.APIS}/></Route>
