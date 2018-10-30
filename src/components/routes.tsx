2d57ab61074cb906d72adf5e1f2fe828a163017b
Dines123 <34677908+Dines123@users.noreply.github.com>
2018-10-30 16:21:10 +0100

    routes

--- src/components/routes.tsx
+++ src/components/routes.tsx
@@ -4,16 +4,19 @@ import BasicComponent from "../types/basicComponent";
 import Register from "../pages/Register";
 import ListTest from "../pages/ListTest";
 import Login from "../pages/LoginPage";
+import Home from "src/pages/HomePage";
+import Products from "src/pages/DetailPage";
 
 export default class Routes extends BasicComponent {
 	public render(){
 		return (
 			<div>
 				<Switch>
-					<Route exact={true} path="/" ><h1>Test</h1></Route>
+					<Route exact={true} path="/" ><Home/></Route>
 					<Route exact={true} path="/Register"><Register APIS={this.props.APIS}/></Route>
 					<Route exact={true} path="/ListTest"><ListTest APIS={this.props.APIS}/></Route>
 					<Route exact={true} path="/Login"><Login APIS={this.props.APIS}/></Route>
+					<Route exact={true} path="/products/:id"><Products APIS={this.props.APIS}/></Route>
 				</Switch>
 
 			</div>
