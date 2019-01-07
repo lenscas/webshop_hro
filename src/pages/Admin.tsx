7878d825b8f51079434729e998b46cd5a142123a
Nigel Huijts <nigelhuijts@hotmail.nl>
2019-01-07 14:19:33 +0100

    Create with error

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -4,16 +4,33 @@ import BasicPage from "src/types/basicComponent";
 
 import { apiUrl } from 'src/config';
 import "../style/admin.css";
+import { APIReturn } from "src/services/basics";
+import { RegisterUserAsAdmin } from "src/services/users";
+import Form, {InputField, FormData} from "../components/form";
+import { retTrue } from "src/funcs/lambdas";
 
-export default class Admin extends BasicPage<props, { render: string }> {
+type registerState = {
+    success? : boolean | {success: boolean, message}
+}
 
-    tabClasses: { [key: string]: string } = { Hangfire: "nav-link active", Edit: "nav-link" }
+export default class Admin extends BasicPage<props, { render: string } & registerState> {
+
+    tabClasses: { [key: string]: string } = { Hangfire: "nav-link active", Edit: "nav-link", Create: "nav-link", Delete: "nav-link"  }
 
     constructor(propsy) {
         super(propsy);
         this.state = {
             render: "Hangfire"
         }
+    }
+    async onSubmit(data : FormData<RegisterUserAsAdmin>){
+        console.log(data)
+        await (
+           this.props.APIS.req.buildRequest("path",`api/admin/users/`)
+           .buildRequest("method", "POST")
+           .buildRequest("body",{user: data.values})
+           .buildRequest("converter",(t:APIReturn<boolean>)=>({success : t.success}))
+       ).run<{success: boolean}>()
     }
 
     renderHangfire() {
@@ -23,6 +40,71 @@ export default class Admin extends BasicPage<props, { render: string }> {
                 <iframe src={apiUrl + "hangfire"} className="hangfire" />
             </div>
         )
+    }
+    renderCreate(){
+
+        if(this.state.success !== true){
+            const fields : InputField[]= [
+                {
+                    type:"text",
+                    validator:retTrue,
+                    name : "name",
+                    label : "Username",
+                    placeholder: "Username",
+                    id : "username",
+                },
+                {
+                    validator:retTrue,
+                    name : "email",
+                    label : "E-mail",
+                    placeholder: "name@example.com",
+                    id : "e-mail",
+                    type: "email"
+                },
+                {
+                    validator:retTrue,
+                    name : "password",
+                    label : "Password",
+                    placeholder: "Password",
+                    id : "password-checker",
+                    type: "password"
+                },
+                {
+                    validator:retTrue,
+                    name : "repeatPassword",
+                    label : "Repeat password",
+                    placeholder: "Repeat password",
+                    id : "password",
+                    type: "password"
+                },
+                {
+                    type:"text",
+                    validator:retTrue,
+                    name : "approach",
+                    label : "Approach",
+                    placeholder: "Mr. / Mrs.",
+                    id : "honorific"
+                },
+                {
+                    type:"text",
+                    validator:retTrue,
+                    name : "role",
+                    label : "Role",
+                    placeholder: "Admin or User",
+                    id : "honorific"
+                },
+                {
+                    name : "Submit",
+                    placeholder: "Submit",
+                    id : "submit",
+                    type : "button"
+                }
+            ]
+            const onSubmit =(data :FormData<RegisterUserAsAdmin> )=>this.onSubmit(data)
+            return <>
+                <Form<RegisterUserAsAdmin> onSubmit={onSubmit} inputs={fields}/>
+            </>
+        }return "Oops, something went wrong, try again later"
     }
 
     setTabOnClick(id: string) {
@@ -46,7 +128,13 @@ export default class Admin extends BasicPage<props, { render: string }> {
                         </li>
 
                         <li className="nav-item">
+                            <button className={this.tabClasses.Create} onClick={this.setTabOnClick("Create")} >Create</button>
+                        </li>
+                        <li className="nav-item">
                             <button className={this.tabClasses.Edit} onClick={this.setTabOnClick("Edit")} >Edit</button>
+                        </li>
+                        <li className="nav-item">
+                            <button className={this.tabClasses.Delete} onClick={this.setTabOnClick("Delete")} >Delete</button>
                         </li>
                     </ul>
                 </div>
@@ -62,8 +150,10 @@ export default class Admin extends BasicPage<props, { render: string }> {
         switch (this.state.render) {
             case "Hangfire":
                 return this.renderHangfire()
+            case "Create":
+                return this.renderCreate()
             case "Edit":
-                return <p>Edit</p>
+                return this.renderCreate()
             default:
                 return <p>No page</p>
         }
